1a19f18436804aad42efb090b05ce0295575b90e
Letian Zhang <letzhang@ebay.com>
2012-07-25 09:52:24 +0800

    add some view controller codes
    ready for view layer and templates

--- protected/controller/view.php
+++ protected/controller/view.php
@@ -1,8 +1,42 @@
 <?php
 class LHControllerView extends JControllerBase
 {
+	/**
+	 * Entry of this controller
+	 * @return boolean success status
+	 */
 	public function execute() {
 		$id = $this->input->getInt('user_id');
-		$this->getApplication()->setBody("View User" . ($id ? " #$id" : ""));
+		if ($id)
+		{
+			$this->view($id);
+		}
+		else
+		{
+			$this->viewAll();
+		}
+		return true;
+	}
+
+	/**
+	 * View single user
+	 * @param  int $id User ID
+	 */
+	public function view($id)
+	{
+		$user = LHModel::getInstance('user');
+		if (!$user->load($id))
+			throw new RuntimeException("User not exists.", 404);
+
+		var_dump($user);
+	}
+
+	/**
+	 * View all users
+	 */
+	public function viewAll()
+	{
+		$users = LHModel::getInstance('user')->findAll();
+		var_dump($users);
 	}
 }
