ae166aa5d09c207719c5b35d9dcb2b4417f6dc26
Letian Zhang <letzhang@ebay.com>
2012-07-26 13:11:31 +0800

    simplify the database schema

--- protected/data/schema.sql
+++ protected/data/schema.sql
@@ -1,46 +1,14 @@
-/*
-Navicat SQLite Data Transfer
-
-Source Server         : joomla-platform-mvc-example
-Source Server Version : 30706
-Source Host           : :0
-
-Target Server Type    : SQLite
-Target Server Version : 30706
-File Encoding         : 65001
-
-Date: 2012-07-24 10:07:16
-*/
-
-PRAGMA foreign_keys = OFF;
-
--- ----------------------------
--- Table structure for "main"."lh_user"
--- ----------------------------
-DROP TABLE "main"."lh_user";
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
 CREATE TABLE "lh_user" (
 "id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 "name"  TEXT NOT NULL
 );
-
--- ----------------------------
--- Records of lh_user
--- ----------------------------
-INSERT INTO "main"."lh_user" VALUES (1, 'Alice');
-INSERT INTO "main"."lh_user" VALUES (2, 'Bob');
-INSERT INTO "main"."lh_user" VALUES (3, 'Rock');
-INSERT INTO "main"."lh_user" VALUES (4, 'Paper');
-INSERT INTO "main"."lh_user" VALUES (5, 'Scissors');
-INSERT INTO "main"."lh_user" VALUES (6, 'Lizard');
-INSERT INTO "main"."lh_user" VALUES (7, 'Spock');
-
--- ----------------------------
--- Table structure for "main"."sqlite_sequence"
--- ----------------------------
-DROP TABLE "main"."sqlite_sequence";
-CREATE TABLE sqlite_sequence(name,seq);
-
--- ----------------------------
--- Records of sqlite_sequence
--- ----------------------------
-INSERT INTO "main"."sqlite_sequence" VALUES ('lh_user', 7);
+INSERT INTO "lh_user" VALUES(1,'Alice');
+INSERT INTO "lh_user" VALUES(2,'Bob');
+INSERT INTO "lh_user" VALUES(3,'Rock');
+INSERT INTO "lh_user" VALUES(4,'Paper');
+INSERT INTO "lh_user" VALUES(5,'Scissors');
+INSERT INTO "lh_user" VALUES(6,'Lizard');
+INSERT INTO "lh_user" VALUES(7,'Spock');
+COMMIT;
